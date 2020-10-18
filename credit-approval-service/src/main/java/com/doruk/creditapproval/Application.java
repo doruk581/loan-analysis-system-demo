29c84cfe90e7dbde614b820e425f2e622c5232ae
Doruk <doruksucs@gmail.com>
2020-10-18 15:55:13 +0300

    eureka-server added

--- credit-approval-service/src/main/java/com/doruk/creditapproval/Application.java
+++ credit-approval-service/src/main/java/com/doruk/creditapproval/Application.java
@@ -10,10 +10,13 @@ import com.doruk.creditapproval.domain.DefaultCreditApprovalService;
 import com.doruk.creditapproval.interfaces.request.CreditApproveRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Configuration
 public class Application {
     public static void main(String[] args) {
--- identity-service/src/main/java/com/doruk/identity/Application.java
+++ identity-service/src/main/java/com/doruk/identity/Application.java
@@ -9,10 +9,14 @@ import com.doruk.identity.infrastructure.externalidentityservice.DefaultExternal
 import com.doruk.identity.infrastructure.externalidentityservice.ExternalIdentityService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @Configuration
 public class Application {
     public static void main(String[] args) {
